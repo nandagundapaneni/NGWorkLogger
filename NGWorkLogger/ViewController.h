c93fb4c2db3b7d5a94c3cf813209304ebbd26ef2
Nanda Gundapaneni <nanda.gundapaneni@markit.com>
2015-10-07 09:45:13 -0600

    Initial Commit

---
+++ NGWorkLogger/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NGWorkLogger
+//
+//  Created by Nanda Gundapaneni on 10/7/15.
+//  Copyright © 2015 NandaKG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
